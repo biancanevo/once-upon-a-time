3621037a90073110e66945167f75c797f93d2340
Marcello Leida <marcello.leida@gmail.com>
2026-01-17 12:22:29 +0100

    add untracked files

---
+++ src/raspi_storyteller/__main__.py
@@ -0,0 +1,11 @@
+"""
+Entry point for running raspi_storyteller as a module.
+
+Allows running the application using:
+    python -m raspi_storyteller
+"""
+
+from raspi_storyteller.app import main
+
+if __name__ == "__main__":
+    main()
